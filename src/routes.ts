80cd13da7a8a4033c39805de4cb751979baf6397
Jackie098 <carlosmiranda19122@gmail.com>
2020-05-12 21:40:36 -0300

    Typing arrays and objects

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld (req: Request, res: Response) {
+  const user = createUser({
+    email: 'diego@rocketseat.com.br',
+    password: '123345',
+    techs: [
+      'Node.js',
+      'ReactJS', 
+      'React Native',
+      { title: 'Javascript', experience: 100},
+    ],
+  });
+
+
   return res.json({ message: 'Hello World!!!'});
 }
